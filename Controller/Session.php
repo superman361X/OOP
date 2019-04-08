41d9b3ccc9e91337311a49501c9937b79bdc9897
zhouc <chao.zhou@seeed.cc>
2019-04-08 09:40:39 +0800

    init

---
+++ Controller/Session.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controller;
+
+
+class Session
+{
+
+    public function cookie()
+    {
+        setcookie('name','zhouc');
+        //http response set-cookie
+    }
+
+
+    public function session()
+    {
+        //request cookie
+        session_start();
+    }
+
+
+    public function token()
+    {
+        session_start();
+        dump($_COOKIE);
+    }
+}
